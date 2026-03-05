89b97dad4a8f67fc76ed4ab62a9e2d869e654e15
Oliver Ah You <ahyouoliver9@gmail.com>
2026-03-05 11:04:01 +0400

    5/3/2026

--- Week5/Week5Day2ExercisesXP.py
+++ Week5/Week5Day2ExercisesXP.py
@@ -136,9 +136,10 @@
 # print("Median:", median_value)
 # print("Mode:", mode_value)
 
+
 # import matplotlib.pyplot as plt
 
-#  # Sample data
+# #  # Sample data
 # SepalLengthCm = [5.1, 4.9, 4.7, 4.6, 5.0]
 # SepalWidthCm = [3.5, 3.0, 3.2, 3.1, 3.6]
 
