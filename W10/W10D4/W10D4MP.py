0aa98d0fcf1515d87bc7fa013ddbac950a598f7e
Oliver Ah You <ahyouoliver9@gmail.com>
2026-04-11 13:10:33 +0400

    11/4/2026

--- W10/W10D4/W10D4MP.py
+++ W10/W10D4/W10D4MP.py
@@ -7,9 +7,9 @@
 # exang:binary
 # slope:ordinal
 # thal:one-hot
-features = ['id', 'age', 'sex', 'dataset', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalch', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'num', 'sex_enc', 'fbs_enc', 'exang_enc', 'cp_asymptomatic', 'cp_atypical angina', 'cp_non-anginal', 'cp_typical angina', 'restecg_lv hypertrophy', 'restecg_normal', 'restecg_st-t abnormality', 'thal_fixed defect', 'thal_normal', 'thal_reversable defect', 'slope_enc']
-X_train = X_train[features]
-X_test = X_test[features]
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# features = ['id', 'age', 'sex', 'dataset', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalch', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'num', 'sex_enc', 'fbs_enc', 'exang_enc', 'cp_asymptomatic', 'cp_atypical angina', 'cp_non-anginal', 'cp_typical angina', 'restecg_lv hypertrophy', 'restecg_normal', 'restecg_st-t abnormality', 'thal_fixed defect', 'thal_normal', 'thal_reversable defect', 'slope_enc']
+# X_train = X_train[features]
+# X_test = X_test[features]
+# scaler = StandardScaler()
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
